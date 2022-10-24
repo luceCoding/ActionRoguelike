003c4f500215623fa302084f495551c1dd1e41a3
Joseph Luce <luceCoding@gmail.com>
2022-10-24 01:45:16 -0700

    Init.

---
+++ Source/ActionRogueLike/ActionRogueLikeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActionRogueLikeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKE_API AActionRogueLikeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
