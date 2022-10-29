f60197864264bebf9c17484da2b50b29e3ad9e9a
Joseph Luce <luceCoding@gmail.com>
2022-10-29 16:38:50 -0700

    Fix projectile.

--- Source/ActionRogueLike/Private/SMagicProjectile.cpp
+++ Source/ActionRogueLike/Private/SMagicProjectile.cpp
@@ -46,7 +46,7 @@ void ASMagicProjectile::Tick(float DeltaTime)
 void ASMagicProjectile::OnActorBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (OtherActor)
+	if (OtherActor && OtherActor != GetInstigator())
 	{
 		if (USAttributeComponent* AttributeComp = Cast<USAttributeComponent>(OtherActor->GetComponentByClass(USAttributeComponent::StaticClass())))
 		{
